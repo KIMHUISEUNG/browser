718d9e17ab21e4eca24f08c16cb5ba2d1dda92b3
hatso <mmorpg3636@naver.com>
2022-03-28 19:57:31 +0900

    shoopingList: refactoring js

--- chapter1/shooppingList/main.js
+++ chapter1/shooppingList/main.js
@@ -20,34 +20,33 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0; //UUID
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+        <div class="item">
+            <span class="item__name">${text}</span>
+            <button class="item__delete">
+                <i class="fas fa-eraser" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item__divider"></div>`;
+  id++;
 
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class="fas fa-eraser"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
   return itemRow;
 }
+// const ul = document.querySelector("ul");
+//   ul.addEventListener("click", (event) => {
+//     if (
+//       event.target.tagName == "li" &&
+//       event.target.className == "item__delete"
+//     ) {
+//       console.log("delete in");
+//       event.target.items.removeChild(itemRow);
+//     }
+//   });
 
 addBtn.addEventListener("click", () => {
   onAdd();
@@ -56,5 +55,13 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
